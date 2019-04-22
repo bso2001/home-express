4dbd488dc89ce965d358086e6224abcc5ddff603
Bert Olsson <bert.olsson@gmail.com>
2019-04-22 17:20:53 -0400

    New background.

--- public/js/global.js
+++ public/js/global.js
@@ -1,6 +1,6 @@
 $( document ).ready( function()
 {
-	$( "div[class*=' popmake-']" ).click( function ()
+	$( "span[class*=' popmake-']" ).click( function()			// modal popup handler
 	{
 		let x = $(this).attr( 'class' );
 		let y = x.substring( x.indexOf( 'popmake-' ) + 8 );
@@ -20,5 +20,21 @@ $( document ).ready( function()
 
 		modal.open();
 		modal.close();
+	});
+
+	$( '.bso-box#go-to-music' ).click( function()				// click handlers for home page boxes
+	{
+		top.location = '/music';
+	});
+
+	$( '.bso-box#go-to-images' ).click( function()
+	{
+		top.location = '/images';
+	});
+
+	$( '.bso-box#go-to-tech' ).click( function()
+	{
+		top.location = '/images';
+		window.open( 'http://olsson.tech', '_new' );
 	});
 });
