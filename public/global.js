f8d428a061df398a67d0de2728d6f45c862ecb6f
Bert Olsson <bert.olsson@gmail.com>
2020-07-19 01:19:12 -0400

    Assets restructured.

---
+++ public/global.js
@@ -0,0 +1,39 @@
+$( document ).ready( function()
+{
+	$( "span[class*=' popmake-']" ).click( function()			// modal popup handler
+	{
+		let x = $(this).attr( 'class' );
+		let y = x.substring( x.indexOf( 'popmake-' ) + 8 );
+		let z = y.substring( 0, y.indexOf( '-' ) );
+		let u = '/lyrics/' + z + '.html';
+
+		let modal = new tingle.modal(
+		{
+			closeMethods: ['overlay', 'escape'],
+			closeLabel: "Close"
+		});
+
+		$.ajax(
+		{
+			url : u, success : function( data ) { modal.setContent( data ); }
+		});
+
+		modal.open();
+		modal.close();
+	});
+
+	$( '.content#go-to-music' ).click( function()				// click handlers for home page boxes
+	{
+		top.location = '/music';
+	});
+
+	$( '.content#go-to-images' ).click( function()
+	{
+		top.location = '/images';
+	});
+
+	$( '.content#go-to-tech' ).click( function()
+	{
+		window.open( 'http://olsson.tech', '_new' );
+	});
+});
